8d1cf31e322017e786af34252acc9a59a8975219
Sam Kane <samuelkane526@gmail.com>
2025-08-06 11:39:43 -0500

    Create psScriptTwo.ps1

---
+++ psScriptTwo.ps1
@@ -0,0 +1,22 @@
+param([string]$text = "Welcome to PowerShell", [int]$speed = 10) # 10ms
+
+function WriteLine([string]$line) {
+	[int]$end = $line.Length
+	$startPos = $HOST.UI.RawUI.CursorPosition
+	$spaces = "                                                                     "
+	[int]$termHalfWidth = 120 / 2
+	foreach($pos in 1 .. $end) {
+		$HOST.UI.RawUI.CursorPosition = $startPos
+		Write-Host "$($spaces.Substring(0, $termHalfWidth - $pos / 2) + $line.Substring(0, $pos))" -noNewline
+		Start-Sleep -milliseconds $speed
+	}
+	Write-Host ""
+}
+
+try {
+	WriteLine $text 
+	exit 0 # success
+} catch {
+        "⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+        exit 1
+}
